75b898e3897597f481e2b0953a60e0635e167bf1
E. Maric <emaric.dev@gmail.com>
2024-11-29 00:14:26 +0800

    :poop: day 7

--- 2021/current/day7.py
+++ 2021/current/day7.py
@@ -45,7 +45,6 @@ def part1(input):
         crab_count_in_pos[crab] += 1
 
     # calculate fuel for each position to max
-    fuel_count = []
     max_pos = max(input)
     min_fuel = None
     for pos in range(max_pos):
@@ -64,7 +63,32 @@ def part1(input):
 
 
 def part2(input):
-    return input
+    # count crabs in position
+    crab_count_in_pos = {}
+    for crab in input:
+        if crab not in crab_count_in_pos:
+            crab_count_in_pos[crab] = 0
+        crab_count_in_pos[crab] += 1
+
+    # calculate fuel for each position to max
+    max_pos = max(input)
+    min_fuel = None
+    for pos in range(max_pos):
+        total_fuel = 0
+        for crab in crab_count_in_pos.keys():
+            start = min(crab, pos)
+            end = max(crab, pos)
+            fuel = start - end
+            fuel = (fuel / 2) * (fuel - 1)
+            fuel *= crab_count_in_pos.get(crab)
+            total_fuel += fuel
+            # print("crab", crab, "fuel", fuel)
+        if min_fuel is None:
+            min_fuel = total_fuel
+        min_fuel = min(min_fuel, total_fuel)
+        # print("pos", pos, "fuel", total_fuel)
+
+    return min_fuel
 
 
 input = cl
