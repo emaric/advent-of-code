1019e980198c541a8f095c4204353fc9a0485049
E. Maric <emaric.dev@gmail.com>
2024-12-01 13:54:18 +0800

    :sparkles: reddit solution

--- 2024/current/day1.py
+++ 2024/current/day1.py
@@ -63,8 +63,16 @@ def part2v2(input):
     return sum([l * counter[l] for l in left])
 
 
+@timer
+def part2_by_4hpq():
+    data = [*map(int, open(locations.input_file).read().split())]
+    A, B = sorted(data[0::2]), sorted(data[1::2])
+    print(sum(map(lambda a, b: abs(a - b), A, B)), sum(a * B.count(a) for a in A))
+
+
 input = cl
 print("part1", part1(input))
 
 print("part2", part2(input))
 print("part2v2", part2v2(input))
+print("_", part2_by_4hpq())
