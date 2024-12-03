eb5c558b65c14935184fd330dff459b1c9eaeae8
E. Maric <emaric.dev@gmail.com>
2024-12-03 17:26:41 +0800

    :poop: part 2 of day 2

--- 2024/current/day2.py
+++ 2024/current/day2.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass, field
 from functools import reduce
 from pprint import pprint
 
+from shared.decorators import timer
 from shared.helpers import Grid, Point, Vectors, get_locations, read_input
 from shared.util import (
     extend_list,
@@ -36,10 +37,14 @@ cl = content.split("\n")
 # cl = cl[3:-4]
 
 
+indices = []
+
+
+@timer
 def part1(input):
     count = 0
     valid_diff = {1, 2, 3, -1, -2, -3}
-    for line in input:
+    for line, i in zip(input, range(len(input))):
         levels = list(map(int, line.split(" ")))
         duplicates = len(levels) - len(set(levels))
         if duplicates <= 0:
@@ -52,10 +57,13 @@ def part1(input):
             )
             if is_all_decreasing_or_increasing:
                 count += 1 if len(set(level_diffs) - valid_diff) == 0 else 0
+                indices.append(i)
+    print(indices)
 
     return count
 
 
+@timer
 def part2(input):
     count = 0
     decreasing_diffs = {1, 2, 3}
@@ -72,24 +80,51 @@ def part2(input):
             return True
         return False
 
-    for line in input:
+    def is_arr_safe(arr):
+        diffs = [a - b for a, b in zip(arr, arr[1:])]
+        is_decreasing = len(set(diffs) - decreasing_diffs) == 0
+        is_increasing = len(set(diffs) - increasing_diffs) == 0
+        return (
+            is_decreasing
+            and (not is_increasing)
+            or is_increasing
+            and (not is_decreasing)
+        )
+
+    for line, i in zip(input, range(len(input))):
         levels = list(map(int, line.split(" ")))
         unsafe_sequences = []
+
+        def unsafe_sequences_count():
+            return len(unsafe_sequences)
+
         for a, b, c in zip(levels, levels[1:], levels[2:]):
             if not is_safe(a, b, c):
                 unsafe_sequences.append([a, b, c])
-                unsafe_sequences_count = len(unsafe_sequences)
-                if unsafe_sequences_count > 2:
+                if unsafe_sequences_count() > 2:
                     break
-        if len(unsafe_sequences) > 2:
-            pass
-        elif len(unsafe_sequences) == 2:
-            print("----------", levels)
-            a, b, c = unsafe_sequences[0]
-            d = unsafe_sequences[1][2]
-            print("unsafe_sequences", unsafe_sequences, a - b, b - c, c - d)
-        else:
+        # if is_arr_safe(levels):
+        if unsafe_sequences_count() == 0:
             count += 1
+        else:
+            # test = [a - b for a, b in zip(levels, levels[1:])]
+            # if is_arr_safe(levels[1:]) or is_arr_safe(levels[:-1]):
+            #     count += 1
+            if unsafe_sequences_count() == 1:
+                if is_arr_safe(levels[1:]) or is_arr_safe(levels[:-1]):
+                    count += 1
+                    # print("safe", levels, test)
+                    pass
+            #     else:
+            #         # print("unsafe", levels, test)
+            #         pass
+            elif unsafe_sequences_count() == 2:
+                # print(set(test) - decreasing_diffs, set(test) - increasing_diffs)
+                for _i in range(len(levels)):
+                    if is_arr_safe([levels[_] for _ in range(len(levels)) if _ != _i]):
+                        count += 1
+                        break
+                pass
 
     return count
 
