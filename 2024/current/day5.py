7640e5235da8ba25271df32b752d5a1a4e107708
E. Maric <emaric.dev@gmail.com>
2024-12-05 19:04:42 +0800

    :poop: day 5, part 2

--- 2024/current/day5.py
+++ 2024/current/day5.py
@@ -2,7 +2,7 @@ import math
 import re
 from collections import Counter
 from dataclasses import dataclass, field
-from functools import reduce
+from functools import cmp_to_key, reduce
 from pprint import pprint
 
 from shared.decorators import timer
@@ -56,8 +56,7 @@ def part1(input):
                         return False
         return True
 
-    for update in updates:
-        pages = list(map(int, update.split(",")))
+    for pages in updates:
         if _is_correctly_ordered(rules, pages):
             mid = pages[int(len(pages) / 2)]
             mid_pages_sum += mid
@@ -65,9 +64,72 @@ def part1(input):
     return mid_pages_sum
 
 
+def _sort(arr, comparator):
+    if len(arr) > 1:
+        if len(arr) > 2:
+            mid_index = int(len(arr) / 2) + 1
+            left = _sort(arr[:mid_index], comparator)
+            right = _sort(arr[mid_index:], comparator)
+        else:
+            left = [arr[0]]
+            right = [arr[1]]
+        merged = []
+
+        _r = 0
+        _l = 0
+
+        while _l < len(left) and _r < len(right):
+            if comparator(left[_l], right[_r]):
+                merged.append(left[_l])
+                _l += 1
+            else:
+                merged.append(right[_r])
+                _r += 1
+
+        for i in range(_l, len(left)):
+            merged.append(left[i])
+
+        for i in range(_r, len(right)):
+            merged.append(right[i])
+
+        return merged
+
+    else:
+        return arr
+
+
 @timer
 def part2(input):
-    return ""
+    mid_pages_sum = 0
+    rules, updates = input
+
+    def _is_correctly_ordered(_rules, _pages):
+        relevant_rules = []
+        for page in _pages:
+            relevant_rules.append(_rules[page])
+
+        # print(pages, "relevant rules:", relevant_rules)
+        for rules in relevant_rules:
+            for x, y in rules:
+                if x in pages and y in pages:
+                    x_index = pages.index(x)
+                    y_index = pages.index(y)
+                    if x_index > y_index:
+                        return False
+        return True
+
+    def _is_printed_before_target(page, target):
+        for x, y in rules[page]:
+            if x == target and y == page:
+                return False
+        return True
+
+    for pages in updates:
+        if not _is_correctly_ordered(rules, pages):
+            corrected = _sort(pages, _is_printed_before_target)
+            mid_pages_sum += corrected[int(len(corrected) / 2)]
+
+    return mid_pages_sum
 
 
 input = cl
@@ -85,6 +147,10 @@ for x, y in rules:
 
 rules = rules_dict
 updates = input[input.index("") + 1 :]
+updates = [list(map(int, update.split(","))) for update in updates]
 input = [rules, updates]
 print("part1", part1(input))
+
+# failed: 4917 -> answer is too low
+# failed: 4946 -> answer is too low
 print("part2", part2(input))
