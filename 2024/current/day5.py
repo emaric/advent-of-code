4af76948b83f642081b2c6f886354918c0de38fa
E. Maric <emaric.dev@gmail.com>
2024-12-05 13:54:01 +0800

    :sparkles: day 5, part 1

--- 2024/current/day5.py
+++ 2024/current/day5.py
@@ -29,23 +29,62 @@ logger_enable(log, f"day{DAY}")
 locations = get_locations(f"day{DAY}")
 
 
-content = read_input(locations.example_file)
-# content = read_input(locations.input_file)
+# content = read_input(locations.example_file)
+content = read_input(locations.input_file)
 
 cl = content.split("\n")
-cl = cl[3:-4]
+# cl = cl[3:-4]
 
 
 @timer
 def part1(input):
-    return input
+    mid_pages_sum = 0
+    rules, updates = input
+
+    def _is_correctly_ordered(_rules, _pages):
+        relevant_rules = []
+        for page in _pages:
+            relevant_rules.append(_rules[page])
+
+        # print(pages, "relevant rules:", relevant_rules)
+        for rules in relevant_rules:
+            for x, y in rules:
+                if x in pages and y in pages:
+                    x_index = pages.index(x)
+                    y_index = pages.index(y)
+                    if x_index > y_index:
+                        return False
+        return True
+
+    for update in updates:
+        pages = list(map(int, update.split(",")))
+        if _is_correctly_ordered(rules, pages):
+            mid = pages[int(len(pages) / 2)]
+            mid_pages_sum += mid
+
+    return mid_pages_sum
 
 
 @timer
 def part2(input):
-    return input
+    return ""
 
 
 input = cl
+rules = input[: input.index("")]
+rules = [list(map(int, rule.split("|"))) for rule in rules]
+
+rules_dict = {}
+for x, y in rules:
+    if x not in rules_dict:
+        rules_dict[x] = []
+    if y not in rules_dict:
+        rules_dict[y] = []
+    rules_dict[x].append((x, y))
+    rules_dict[y].append((x, y))
+
+rules = rules_dict
+updates = input[input.index("") + 1 :]
+input = [rules, updates]
 print("part1", part1(input))
 print("part2", part2(input))
