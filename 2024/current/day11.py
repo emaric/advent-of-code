f815f608c756cffb89102dc9b362f7aa9a0a0b6b
E. Maric <emaric.dev@gmail.com>
2024-12-15 03:32:36 +0800

    :sparkles: day 11

---
+++ 2024/current/day11.py
@@ -0,0 +1,119 @@
+from __future__ import annotations
+
+import enum
+import math
+import re
+from collections import Counter, defaultdict
+from dataclasses import dataclass, field
+from functools import reduce
+from pprint import pprint
+from typing import Any, ItemsView, Type
+
+from shared.decorators import timer
+from shared.helpers import Point, Vectors, get_locations, read_input
+from shared.util import (
+    extend_list,
+    extend_list_2D,
+    extend_list_rect,
+    ints,
+    log,
+    logger_config,
+    logger_enable,
+    logger_init,
+    print_array,
+    strs,
+    wait_for_input,
+)
+
+DAY = 11
+
+logger_init()
+logger_enable(log, f"day{DAY}")
+
+locations = get_locations(f"day{DAY}")
+
+example_content = read_input(locations.example_file)
+example_cl = example_content.split("\n")
+# example_cl = example_cl[3:-4]
+
+content = read_input(locations.input_file)
+cl = content.split("\n")
+
+
+class Stones:
+    def __init__(self, input: str):
+        self.transforms: dict[int : [list[int]]] = {}
+
+        input_int = list(map(int, input.split(" ")))
+        self.stones: Counter[int:int] = Counter(input_int)
+
+    def _calc_blink(self, stone_value: int) -> list[int]:
+        """ "returns output of this stone after a blink"""
+        if stone_value in self.transforms.keys():
+            return self.transforms[stone_value]
+        else:
+            # 0 => 1
+            if stone_value == 0:
+                return [1]
+            # even number of digits => split into 2
+            if len(str(stone_value)) % 2 == 0:
+                s = str(stone_value)
+                midpoint = int(len(s) / 2)
+                return [int(s[:midpoint]), int(s[midpoint:])]
+            # default, *= 2024
+            return [stone_value * 2024]
+
+    def count_stones_after_blinks(self, blinks: int):
+        # calc blink for each stone in self.stones
+
+        for i in range(blinks):
+            print(f"on blink# {i}")
+            new_counter = Counter()
+            stone_copy = self.stones.items()
+            for stone_value, quantity in stone_copy:
+                if quantity == 0:
+                    continue
+                transformation_list = self._calc_blink(stone_value)
+
+                for sv in transformation_list:
+                    new_counter[sv] += quantity
+
+                # new_counter.update(transformation_list)
+
+            self.stones = new_counter
+            # self._print_stones()
+        return self.stones.total()
+
+    def _print_stones(self):
+        print([_ for _ in self.stones.elements()])
+
+
+@timer
+def part1(input):
+    s = Stones(input)
+    return s.count_stones_after_blinks(25)
+
+
+@timer
+def part2(input):
+    s = Stones(input)
+    return s.count_stones_after_blinks(75)
+
+
+print("------------------------------------------------------------")
+print("EXAMPLE INPUT")
+print("------------------------------------------------------------")
+example_input = example_content
+print(part1(example_input))
+print()
+print(part2(example_input))
+
+print()
+print()
+print("------------------------------------------------------------")
+print("REAL INPUT")
+print("------------------------------------------------------------")
+input = content
+print(part1(input))
+print()
+print(part2(input))
--- 2024/current/tester.py
+++ 2024/current/tester.py
@@ -40,107 +40,14 @@ content = read_input(locations.input_file)
 cl = content.split("\n")
 
 
-class Map:
-    N = Point(0, -1)
-    E = Point(1, 0)
-    S = Point(0, 1)
-    W = Point(-1, 0)
-    dirs = [N, E, S, W]
-
-    def __init__(self, input: list[str]):
-        self.input = input
-
-        self.g: dict[Point:int] = defaultdict(int)
-        self.trailheads: list[Trail] = []
-        self.rows: int = len(input)
-        self.cols: int = len(input[0])
-
-        for col, line in enumerate(input):
-            for row, n in enumerate(line):
-                _p = Point(col, row)
-                self.g[_p] = int(n)
-                if n == "0":
-                    self.trailheads.append(Trail(_p))
-
-    def part_one(self):
-        for th in self.trailheads:
-            self.check_trailhead_part_one(th)
-
-    def part_two(self):
-        for th in self.trailheads:
-            self.check_trailhead_part_two(th)
-
-    def check_trailhead_part_one(self, trail: Trail):
-        to_check: list[Point] = []
-        checked: set[Point] = set()
-
-        to_check = self._find_valid_neighbours(trail.p)
-        while len(to_check) > 0:
-            current = to_check.pop(-1)
-            if current in checked:
-                continue
-            checked.add(current)
-
-            if self.g[current] == 9:
-                trail.score += 1
-            to_check.extend(self._find_valid_neighbours(current))
-
-    def check_trailhead_part_two(self, trail: Trail):
-        to_check: list[Point] = []
-
-        to_check = self._find_valid_neighbours(trail.p)
-        while len(to_check) > 0:
-            current = to_check.pop(-1)
-
-            if self.g[current] == 9:
-                trail.score += 1
-            to_check.extend(self._find_valid_neighbours(current))
-
-    def _find_valid_neighbours(self, p: Point):
-        """gets list of points that surround this point that are valid for the next step in the trail"""
-        return [p + vec for vec in Map.dirs if self.g.get((p + vec)) == self.g[p] + 1]
-
-    def score(self):
-        sum = 0
-        for th in self.trailheads:
-            sum += th.score
-        return sum
-
-    def __str__(self):
-        listmap: list[list[int]] = [
-            [0 for c in range(self.rows)] for r in range(self.cols)
-        ]
-
-        p: Point
-        value: str
-        for p, value in self.g.items():
-            listmap[p.col][p.row] = str(value)
-
-        listmap_str = ""
-        for line in listmap:
-            listmap_str += "".join(line)
-            listmap_str += "\n"
-        return listmap_str
-
-
-class Trail:
-    def __init__(self, point: Point):
-        self.p = point
-        self.score: int = 0
-
-
 @timer
 def part1(input):
-    m = Map(input)
-    m.part_one()
-    return m.score()
+    return input
 
 
 @timer
 def part2(input):
-    m = Map(input)
-    m.part_two()
-    return m.score()
+    return ""
 
 
 print("------------------------------------------------------------")
